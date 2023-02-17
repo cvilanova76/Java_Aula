35066629ae679c424bb46f55f2dad885d6158cb6
cvilanova76 <carolinavilanova@gmail.com>
2023-02-17 18:21:49 -0300

    exercicio 1

--- src/tiposprimitivos/Exercicios.java
+++ src/tiposprimitivos/Exercicios.java
@@ -1,5 +1,7 @@
 package tiposprimitivos;
 
+import java.util.Scanner;
+
 public class Exercicios {
     public static void main(String[] args) {
         /* 1 - Crie uma classe que receba o valor de um produto e a 
@@ -7,6 +9,25 @@ public class Exercicios {
         e o valor do produto com o desconto. Observação: o valor do desconto 
         é calculado por meio da fórmula: 
         valor do desconto = valor do produto * percentual de desconto / 100. */
+
+       Scanner scan = new Scanner(System.in);
+
+       System.out.println("Digite o valor do produto");
+       double valor = scanner.nextDouble();
+
+       System.out.println("Digite o percentual de desconto");
+        double percentual = scanner.nextDouble();
+
+        double desconto = valor*(percentual/100);
+
+        double valorFinal = valor - desconto;
+
+        System.out.println("O valor final com desconto é: " + valorFinal);
+
+        scanner.close();
+    }
+}
+
 
 
 
